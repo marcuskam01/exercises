1e328ecd99b41227a1db7a5c6f00e4f44d99c768
Marcus Kam <82490108+marcuskam01@users.noreply.github.com>
2024-10-06 16:44:59 -0400

    solved 1193

---
+++ Database/1193.sql
@@ -0,0 +1,3 @@
+select date_format(t.trans_date, '%Y-%m')as month, country, count(*) as trans_count, count(case when state = 'approved' then 1 else null end) as approved_count, 
+sum(amount) as trans_total_amount, sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from Transactions t 
